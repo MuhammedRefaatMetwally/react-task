7d6028fa0b60303406ad9ea26bd154cde24a7628
Muhammed <muhammedrefaat2020@gmail.com>
2025-06-24 21:02:12 +0300

    go push

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,5 @@
 import { useTheme } from "next-themes"
-// @ts-ignore
-import { Toaster as Sonner, type ToasterProps } from "sonner"
+import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
